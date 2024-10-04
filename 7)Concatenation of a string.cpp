fedec52aaed7bd3d6e6307e0c345eae69e75d313
DhanrajjChavan <dhanarajchavan777@gmail.com>
2024-10-05 00:52:35 +0530

    Update 7)Concatenation of a string.cpp

--- 7)Concatenation of a string.cpp
+++ 7)Concatenation of a string.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
     string first_name,last_name;
-    first_name = "Soham";
-    last_name = "Jade";
+    first_name = "Dhanraj";
+    last_name = "Chavan";
 
     cout<<first_name+last_name;
 }
 
 /*
-SohamJade
+DhanrajChavan
 */
